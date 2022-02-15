3628f14e248e1afa74580dc2dce14d78fb61c9bf
E-Aston <80836421+E-Aston@users.noreply.github.com>
2022-02-15 05:43:27 +0000

    Update geometric_measures.py

--- geometric_measures.py
+++ geometric_measures.py
@@ -18,6 +18,12 @@ def geometric_measures (file):
 
     dict =(ms.compute_geometric_measures())  # Compute measures of closed mesh
     mesh_volume = dict['mesh_volume']  # Assigns mesh volume to variable mesh_volume
+    
+    boundingbox = ms.current_mesh().bounding_box()
+    width = boundingbox.dim_x()
+
+
+    height = boundingbox.dim_z()
 
     ms.load_filter_script('Filter_script.mlx')  # Loads the filter script (this one makes a convex hull)
     ms.apply_filter_script()  # Applies script
@@ -30,7 +36,7 @@ def geometric_measures (file):
     PrOcc = (mesh_volume / cvh_volume) # Proportion of cvh occupied by mesh (compactness)
     SSF = (ASR / mesh_sa) # Shelter size factor (fragmentation of habitat)
 
-    value_list = [str(file), mesh_sa, mesh_volume, cvh_volume, ASR, PrOcc, SSF]
+    value_list = [str(file), mesh_sa, mesh_volume, cvh_volume, ASR, PrOcc, SSF, width, height]
 
     with open("Geometric Measures.csv", "a", newline='') as f:
         write = csv.writer(f)
